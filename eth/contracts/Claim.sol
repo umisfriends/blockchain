e3979d51a2012b11c254aa426ab662d79db72481
umisfriends <umisfriends@outlook.com>
2022-12-07 17:29:40 +0800

    add claim

--- eth/contracts/Claim.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.8.0;
-import "./openzeppelin-contracts/contracts/access/Ownable.sol";
-import "./openzeppelin-contracts/contracts/.sol";
-
-contract Claim is Ownable{
-    
-}
---
+++ eth/contracts/Claim1155.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.0;
+import "./openzeppelin-contracts/contracts/access/Ownable.sol";
+import "./openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+
+interface I1155{
+    function mint(address to, uint256 id, uint256 amount, bytes memory data) external;
+}
+
+contract Claim1155 is Ownable{
+    address public signer;
+    mapping(address => uint256) public nonces;
+    event Claimed(address indexed to, uint256 indexed nonce, address token, uint256 id, uint256 amount);
+    
+    constructor(address _signer){
+        signer = _signer;
+    }
+    
+    function setSigner(address _signer) external onlyOwner{
+        signer = _signer;
+    }
+    
+    function claim(address token, uint256 id, uint256 amount, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external{
+        address to = msg.sender;
+        uint256 nonce = nonces[to];
+        require(block.timestamp <= deadline, "timeout");
+        bytes32 hash = keccak256(abi.encodePacked(block.chainid, address(this), "claim", to, token, id, amount, nonce, deadline));
+        require(ECDSA.recover(hash, v, r, s) == signer, "sign error");
+        I1155(token).mint(to, id, amount, "");
+        emit Claimed(to, nonce, token, id, amount);
+        nonces[to]++;
+    }
+}
